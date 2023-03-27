3cfe501c74b1da70c894649d6a8d9dcfedee56e2
BPuH3A <124492772+BPuH3A@users.noreply.github.com>
2023-03-27 23:10:11 +0300

    Update connectDB.php

--- vendor/connectDB.php
+++ vendor/connectDB.php
@@ -1,6 +1,6 @@
 <?php
 
-$connect = mysqli_connect('localhost', 'root', 'root', 'accounts');
+$connect = mysqli_connect('sql7.freesqldatabase.com', 'sql7609028', 'XeXy18wQPr', 'sql7609028');
 
 if (!$connect) {
     die('Error connect to DataBase');
